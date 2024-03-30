29c44bbfee113976a1f6540eef02d19c52239231
dmirain <dmirain@gmail.com>
2024-03-30 17:53:38 +0300

    Добавил комент

--- FakeNFT/Foundation/StatLog.swift
+++ FakeNFT/Foundation/StatLog.swift
@@ -45,6 +45,8 @@ enum LogEvent {
     }
 }
 
+
+// Наследуемся от этого класса, что бы контроллер автоматом писал в метрику.
 class StatLoggedUIViewController: UIViewController {
     let statLog: StatLog
 
