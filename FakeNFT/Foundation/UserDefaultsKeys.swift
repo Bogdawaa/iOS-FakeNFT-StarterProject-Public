47329c2737dbabfd189eaed4617be95a132150d1
Bogdawaa <bogdan6246@yandex.ru>
2024-04-04 19:49:38 +0300

    Добавлено сохранение параметра сортировки в userdefaults.

---
+++ FakeNFT/Foundation/UserDefaultsKeys.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultsStorage.swift
+//  FakeNFT
+//
+//  Created by Bogdan Fartdinov on 04.04.2024.
+//
+
+import Foundation
+
+enum UserDefaultsKeys: String {
+    case sortParam = "sortParam"
+}
