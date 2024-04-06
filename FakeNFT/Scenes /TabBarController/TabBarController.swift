b0ce5cd69d15f65d392f821217b339941da9cecb
Anna <sozontova.an@icloud.com>
2024-04-06 15:50:53 +0300

    Car_1/3epic

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -1,25 +1,27 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-
+    
     var servicesAssembly: ServicesAssembly!
-
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(systemName: "square.stack.3d.up.fill"),
-        tag: 0
+    
+    private let cartTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.cart", comment: ""),
+        image: UIImage(named: "Add Cart"),
+        tag: 2
     )
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let catalogController = TestCatalogViewController(
-            servicesAssembly: servicesAssembly
-        )
-        catalogController.tabBarItem = catalogTabBarItem
-
-        viewControllers = [catalogController]
-
+        
+        let cartService = CartServiceImpl(networkClient: DefaultNetworkClient())
+        let cartModel = CartViewModel(service: cartService)
+        let cartController = CartViewController(viewModel: cartModel)
+        cartController.tabBarItem = cartTabBarItem
+        
+        viewControllers = [cartController]
+        view.backgroundColor = UIColor(named: "YP White")
+        tabBar.tintColor = UIColor(named: "YP Blue")
+        tabBar.unselectedItemTintColor = UIColor(named: "YP Black")
         view.backgroundColor = .systemBackground
     }
 }
