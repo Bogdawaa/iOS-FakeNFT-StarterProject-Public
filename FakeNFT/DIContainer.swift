20d94096c71b69aa4fd78f1e79eec416f12ccb97
Bogdawaa <bogdan6246@yandex.ru>
2024-04-09 21:01:37 +0300

    Зарегистрированы UserCardViewController и UserCardPresenter

--- FakeNFT/DIContainer.swift
+++ FakeNFT/DIContainer.swift
@@ -7,6 +7,7 @@ final class DIContainer {
         registerFoundation()
         registerCatalog()
         registerStatistics()
+        registerUserCard()
 
         container.register(TabBarController.self) { diResolver in
             TabBarController(
@@ -19,6 +20,10 @@ final class DIContainer {
 
     func tabBarController() -> TabBarController {
         container.resolve(TabBarController.self)!
+    }
+
+    func userCardController() -> UserCardViewController {
+        container.resolve(UserCardViewController.self)!
     }
 
     private func registerCatalog() {
@@ -69,6 +74,21 @@ final class DIContainer {
         container.register(StatisticsViewController.self) { diResolver in
             StatisticsViewController(
                 presenter: diResolver.resolve(StatisticsPresenter.self)!,
+                statlog: diResolver.resolve(StatLog.self)!
+            )
+        }
+        .inObjectScope(.container)
+    }
+
+    private func registerUserCard() {
+        container.register(UserCardPresenter.self) { _ in
+            UserCardPresenter()
+        }
+        .inObjectScope(.container)
+
+        container.register(UserCardViewController.self) { diResolver in
+            UserCardViewController(
+                presenter: diResolver.resolve(UserCardPresenter.self)!,
                 statlog: diResolver.resolve(StatLog.self)!
             )
         }
