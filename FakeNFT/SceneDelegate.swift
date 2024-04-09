4af6c34a6bb566dfe9719df0b7bc4a5d3833566a
Bogdawaa <bogdan6246@yandex.ru>
2024-04-09 20:59:34 +0300

    Обернул начальный tabbar в navigationController

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -13,7 +13,8 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
-        window?.rootViewController = diContainer.tabBarController()
+        let navigationController = UINavigationController(rootViewController: diContainer.tabBarController())
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 }
