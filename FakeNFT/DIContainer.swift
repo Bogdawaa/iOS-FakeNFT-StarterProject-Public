738a2b9b27edb4cfe21da8074952284c45fb9f94
Bogdawaa <bogdan6246@yandex.ru>
2024-04-12 20:59:55 +0300

    StatisticsViewController убран из di контейнера.

--- FakeNFT/DIContainer.swift
+++ FakeNFT/DIContainer.swift
@@ -13,7 +13,10 @@ final class DIContainer {
                 servicesAssembly: diResolver.resolve(ServicesAssembly.self)!,
                 catalogViewController: diResolver.resolve(CatalogViewController.self)!,
                 statisticsController: UINavigationController(
-                    rootViewController: diResolver.resolve(StatisticsViewController.self)!
+                    rootViewController: StatisticsViewController(
+                        presenter: diResolver.resolve(StatisticsPresenter.self)!,
+                        statlog: diResolver.resolve(StatLog.self)!
+                    )
                 )
             )
         }
@@ -64,14 +67,6 @@ final class DIContainer {
                     networkClient: diResolver.resolve(NetworkClient.self)!,
                     storage: UsersStorageImpl()
                 )
-            )
-        }
-        .inObjectScope(.container)
-
-        container.register(StatisticsViewController.self) { diResolver in
-            StatisticsViewController(
-                presenter: diResolver.resolve(StatisticsPresenter.self)!,
-                statlog: diResolver.resolve(StatLog.self)!
             )
         }
         .inObjectScope(.container)
