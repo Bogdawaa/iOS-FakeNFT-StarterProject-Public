5bd7f8cf6c56ef8bd828f169bd3afdcdb985394e
Anna <sozontova.an@icloud.com>
2024-04-17 23:02:17 +0300

    test

--- FakeNFT/Helpers/String+Extension.swift
+++ FakeNFT/Helpers/String+Extension.swift
@@ -1,8 +1,6 @@
 import Foundation
 
-// swiftlint:disable nslocalizedstring_key
 postfix operator ~
 postfix func ~ (string: String) -> String {
     NSLocalizedString(string, comment: "")
 }
-// swiftlint:enable nslocalizedstring_key
