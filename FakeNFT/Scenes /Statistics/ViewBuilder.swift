d10cea928ad11457a70212d553402241055ef69e
Bogdawaa <bogdan6246@yandex.ru>
2024-04-22 01:36:56 +0300

    Подчистил комментарии

--- FakeNFT/Scenes /Statistics/ViewBuilder.swift
+++ FakeNFT/Scenes /Statistics/ViewBuilder.swift
@@ -7,25 +7,6 @@
 
 import UIKit
 
-// final class UserCardRouter {
-//    private var user: User
-//    private var userCardViewController: UserCardViewProtocol
-//    private var webViewPresenter: WebViewPresenter
-//    private var webViewController: WebViewController
-//
-//    init(userCardViewController: UserCardViewProtocol, user: User) {
-//        self.user = user
-//        self.userCardViewController = userCardViewController
-//        self.webViewPresenter = WebViewPresenter(user: user)
-//        self.webViewController = WebViewController(presenter: webViewPresenter)
-//        self.webViewPresenter.view = webViewController
-//    }
-//
-//    func showNextController() {
-//        userCardViewController.showView(viewController: webViewController)
-//    }
-// }
-
 protocol Builder {
     static func buildWebViewController(with user: User) -> UIViewController
 }
