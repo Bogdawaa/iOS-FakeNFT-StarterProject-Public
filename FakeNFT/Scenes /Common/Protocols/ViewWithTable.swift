65cdb1fe72f80030d1232a2e43d5d1d5accb1df3
dmirain <dmirain@gmail.com>
2024-04-30 13:28:04 +0300

    Вынес ViewWithTable в Foundation

---
+++ FakeNFT/Scenes /Common/Protocols/ViewWithTable.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+protocol ViewWithTable {
+    var tableView: UITableView { get set }
+    func reload()
+    func update(newIndexes: Range<Int>)
+}
+
+extension ViewWithTable {
+    func reload() {
+        tableView.reloadData()
+    }
+
+    func update(newIndexes: Range<Int>) {
+        if newIndexes.isEmpty { return }
+        tableView.performBatchUpdates {
+            let indexPaths = newIndexes.map { index in
+                IndexPath(row: index, section: 0)
+            }
+            tableView.insertRows(at: indexPaths, with: .automatic)
+        } completion: { _ in
+        }
+    }
+}
